aa5c6cd5c387125b8fd5105f1410ba1d90c1a6fb
Konrad <konradjones@outlook.com>
2023-11-17 23:26:17 +0100

    add peer review

--- trz.c
+++ trz.c
@@ -1,107 +1,142 @@
-// rozwiązanie brute w O(n^2)
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <limits.h>
+#include <stdbool.h>
 
-#define MAX(x,y) ((x) > (y)? (x) : (y))
-#define MIN(x,y) ((x) < (y)? (x) : (y))
+int min(int a, int b){
+    if(a < b){return a;}
+    else{return b;}
+}
 
-typedef struct Motel {
-    int siec;
-    int odl;
-} Motel;
+int max(int a, int b){
+    if(a > b){return a;}
+    else{return b;}
+}
 
-int main() {
-    int n;
-    scanf("%d", &n);
-
-    Motel* motele = malloc((unsigned) n * sizeof(Motel));
-
-    // wczytujemy motele
-    for (int i = 0; i < n; i++)
-        scanf("%d %d", &motele[i].siec, &motele[i].odl);
-
-    int min = INT_MAX;
-
-    // Liczymy min:
-    // dla każdej pary moteli a,b różnych sieci w odległości mniejszej 
-    // od aktualne_min, szukamy motelu c, w odległości od b mniejszej
-    // od aktualne_min, wtedy aktualne_min to max z tych odległości.
-    for(int a = 0; a < n; a++){
-        for(int b = a+1; b < n; b++){
-
-            const int odlAB = motele[b].odl - motele[a].odl;
-
-            if(odlAB >= min)
-                break;
-
-            if(motele[a].siec == motele[b].siec)
-                continue;
-
-            for(int c = b+1; c < n; c++){
-
-                const int odlBC = motele[c].odl - motele[b].odl;
-
-                if(odlBC >= min)
-                    break;
-
-                if(motele[c].siec != motele[b].siec && motele[c].siec != motele[a].siec)
-                    min = MAX(odlAB, odlBC);
+/*Zwraca odległość najdalszej trójki moteli różnych sieci */
+int min_trzy_rozne(int siec[], int odl[], unsigned n){
+    int mini = INT_MAX;
+    unsigned pierwszy = 0;
+    unsigned i = 1;
+    while(siec[i] == siec[pierwszy]){pierwszy++; i++;}
+    unsigned srodkowy_p = i;
+    unsigned srodkowy_o = i;
+    i++;
+    while(i < n){
+        //jeśli trzy różne sieci
+        if(siec[i] != siec[pierwszy] && siec[i] != siec[srodkowy_p]){
+            for(unsigned j = srodkowy_p; j <= srodkowy_o; j++){
+                mini = min(mini, max(odl[j] - odl[pierwszy], odl[i] - odl[j]));
             }
+            pierwszy = srodkowy_o;
+            srodkowy_p = i;
+            srodkowy_o = i;
+        }
+        //jeśli ta sama co wcześniej
+        else if(siec[i] == siec[pierwszy]){
+            pierwszy = srodkowy_o;
+            srodkowy_p = i;
+            srodkowy_o = i;
+        }
+        else if(siec[i] == siec[srodkowy_p]){
+            srodkowy_o++;
+        }
+        else{
+            return -1;
+        }
+        i++;
+    }
+    return mini;
+}
+
+/*Zwraca minimalną ogległość pomiędzy trzema motelami różnych sieci lub -1 gdy sieć się powtarza*/
+int min_odleglosc(int siec[], int odl[], unsigned a, unsigned b, unsigned c){
+    if(siec[a] == siec[b] || siec[b] == siec[c] || siec[c] == siec[a]){return -1;}
+    return(min(odl[b] - odl[a], odl[c] - odl[b]));
+}
+
+/*Zwraca tablicę n początkowych indeksów moteli, które należą do różnych sieci
+(jeśli nie ma tylu różnych indeksów, pozostałe są wypełnione wartością UINT_MAX) */
+unsigned* poczatkowe_rozne_sieci(int siec[], unsigned rozmiar, unsigned n, bool odwrocone){
+    unsigned* tab = (unsigned*) malloc(n * sizeof(unsigned));
+    for(unsigned i = 0; i < n; i++){tab[i] = UINT_MAX;}
+
+    unsigned licznik_roznych = 0;
+    bool powtorka;
+    unsigned iter;
+    for(unsigned i = 0; i < rozmiar && licznik_roznych < n; i++){
+        //Ustawienie iteratora
+        if(odwrocone){
+            iter = rozmiar - 1 - i;
+        }
+        else{
+            iter = i;
+        }
+        //Sprawdzenie czy różny
+        powtorka = false;
+        for(unsigned j = 0; j < licznik_roznych; j++){
+            if(siec[tab[j]] == siec[iter]){
+                powtorka = true;
+                break;
+            }
+        }
+        //Dodanie gdy różny
+        if(!powtorka){
+            tab[licznik_roznych++] = iter;
+        }
+    }
+    return tab;
+}
+
+/*Zwraca odległość najbliższej trójki moteli różnych sieci */
+int max_trzy_rozne(int siec[], int odl[], unsigned n){
+    unsigned* pierwsze_rozne_sieci = poczatkowe_rozne_sieci(siec, n, 3, false);
+    unsigned* ostatnie_rozne_sieci = poczatkowe_rozne_sieci(siec, n, 3, true);
+    
+    //rozmiar = liczba realnych indeksów w tablicy pierwsze_rozne_sieci
+    unsigned rozmiar = 0;
+    for(int i = 0; i < 3; i++){
+        if(pierwsze_rozne_sieci[i] != UINT_MAX){
+            rozmiar++;
         }
     }
 
-    // jeśli są tylko dwie/jedna sieci to nie zmieni się to INT_MAX więc wtedy wyrzucamy "0 0"
-    if(min == INT_MAX){
-        printf("0 0");
-        return 0;
-    }
+    int maxi = -1;
 
-    /*
-                  ....           ....           ....           ....
-             ||             ||             ||             ||
-         /"""l|\        /"""l|\        /"""l|\        /"""l|\
-        /_______\      /_______\      /_______\      /_______\
-        |  .-.  |------|  .-.  |------|  .-.  |------|  .-.  |------
-         __|L|__| .--. |__|L|__| .--. |__|L|__| .--. |__|L|__| .--.
-        _\  \\p__`o-o'__\  \\p__`o-o'__\  \\p__`o-o'__\  \\p__`o-o'_
-        ------------------------------------------------------------
-        ------------------------------------------------------------
-    */
-
-    int max = 0;
-
-    // Liczymy max:
-    // dla każdej pary moteli a,b różnych sieci w odległości większej 
-    // od aktualne_max, szukamy motelu c, w odległości od b większej
-    // od aktualne_max, wtedy aktualne_max to min z tych odległości.
-    for(int i = 0; i < n; i++){
-        for(int j = n-2; j > i; j--){
-
-            const int odlAB = motele[j].odl - motele[i].odl;
-
-            if(odlAB <= max)
-                break;
-
-            if(motele[i].siec == motele[j].siec)
-                continue;
-
-            for(int k = n-1; k > j; k--){
-
-                const int odlBC = motele[k].odl - motele[j].odl;
-
-                if(odlBC < max)
-                    break;
-
-                if(motele[k].siec != motele[j].siec && motele[k].siec != motele[i].siec)
-                    max = MIN(odlAB, odlBC);
+    /*Iteracja po każdej kombinacji początkowych i końcowych wartości oraz wszystkich pozostałych*/
+    for(unsigned i = 0; i < rozmiar; i++){
+        for(unsigned j = 0; j < rozmiar; j++){
+            for(unsigned k = 0; k < n; k++){
+                maxi = max(maxi, min_odleglosc(siec, odl, pierwsze_rozne_sieci[i], k, ostatnie_rozne_sieci[j]));
             }
         }
     }
+    
+    free(pierwsze_rozne_sieci);
+    free(ostatnie_rozne_sieci);
+    
+    return maxi;
+}
 
-    printf("%d %d", min, max);
+int main(){
+    unsigned n;
+    scanf("%u", &n);
 
-    free(motele);
-    return 0;
+    int* siec = malloc(sizeof(int) * n);
+    int* odl = malloc(sizeof(int) * n);
+
+    for(unsigned i = 0; i < n; i++){
+        scanf("%d %d", &siec[i], &odl[i]);
+    }
+
+    int mini = min_trzy_rozne(siec, odl, n);
+    int maxi = max_trzy_rozne(siec, odl, n);
+
+    if(maxi == -1){
+        //Gdy nie ma 3 różnych elementów
+        printf("%d %d\n", 0, 0);
+    }
+    else{
+        printf("%d %d\n", mini, maxi);
+    }
 }
